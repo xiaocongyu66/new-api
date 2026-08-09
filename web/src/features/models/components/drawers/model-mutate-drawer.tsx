225663b5d51b0c1d91d9005c703847a00e14f9f5
hathawayANdRX105 <2635254302@qq.com>
2026-08-10 00:11:08 +0800

    chore(frontend): remove io.net model settings

--- web/src/features/models/components/drawers/model-mutate-drawer.tsx
+++ web/src/features/models/components/drawers/model-mutate-drawer.tsx
@@ -342,8 +342,6 @@ export function ModelMutateDrawer({
       'channel_affinity_setting.max_entries': 100000,
       'channel_affinity_setting.default_ttl_seconds': 3600,
       'channel_affinity_setting.rules': '[]',
-      'model_deployment.ionet.api_key': '',
-      'model_deployment.ionet.enabled': false,
     }
     return getOptionValue(systemOptionsData.data, defaultModelSettings)
   }, [systemOptionsData])
