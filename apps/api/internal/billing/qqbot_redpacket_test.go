21c9eeaf0b14d8f928e2ea4842126bdc967abefa
hathawayANdRX105 <2635254302@qq.com>
2026-09-11 00:15:05 +0800

    test(qqbot): drop unused variable flagged by go vet

--- apps/api/internal/billing/qqbot_redpacket_test.go
+++ apps/api/internal/billing/qqbot_redpacket_test.go
@@ -66,8 +66,7 @@ func TestHandleRedPacketListShowsStatusAndGrabButtons(t *testing.T) {
 	defer setupRedPacketTestDB(t)()
 
 	active := createTestRedPacket(t, "GROUP_1", "OPENID_A", 5, 3, billing.RedPacketStatusActive, 1000)
-	finished := createTestRedPacket(t, "GROUP_1", "OPENID_B", 3, 0, billing.RedPacketStatusFinished, 2000)
-	createTestRedPacket(t, "GROUP_1", "OPENID_C", 2, 2, billing.RedPacketStatusExpired, 3000)
+	createTestRedPacket(t, "GROUP_1", "OPENID_B", 3, 0, billing.RedPacketStatusFinished, 2000)
 
 	content, kb := billing.HandleRedPacketList("GROUP_1")
 	// 倒序:expired 最新,active 最老
